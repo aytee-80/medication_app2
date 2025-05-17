0b38e406e3e676647d8d8adf70cb14452187b5b2
aytee-80 <amogelangthompson@gmail.com>
2025-05-17 13:43:47 +0200

    Initial commit

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # config.py
-DB_HOST = "192.168.0.44"
+DB_HOST = "dpg-d0k6omre5dus73bgro6g-a.oregon-postgres.render.com"
 DB_USER = "aytee7"
-DB_PASSWORD = "Abcdefg123"
+DB_PASSWORD = "LqpUqXEqCQ78jxDG7nc0Rk3qFHoDPOm0"
 DB_NAME = "medicatiion_app"
+DB_PORT = 5432
 SECRET_KEY = "Siya123"
